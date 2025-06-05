6ec14a73604ed1011e50f1b6fea843c439843738
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-06-05 12:43:22 +0600

    add

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Auth routes
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::get('me', [AuthController::class, 'me'])->middleware('auth:api');
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
+// Route::post('refresh', [AuthController::class, 'refresh'])->middleware('auth:api');
+
+
+
+Route::post('password/email', [AuthController::class, 'sendResetOTP']);
+Route::post('password/verify-otp', [AuthController::class, 'verifyResetOTP'])->name('password.verify-otp');
+Route::post('password/reset', [AuthController::class, 'passwordReset'])->name('password.reset');
