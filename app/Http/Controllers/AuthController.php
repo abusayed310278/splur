9ff0927a7a1ec36c88765ce62cd9972b201d8138
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-06-07 13:51:38 +0600

    add

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -128,7 +128,7 @@ class AuthController extends Controller
     public function register(Request $request)
     {
 
-        dd($request->all());
+        // dd($request->all());
         try {
             $validator = Validator::make($request->all(), [
                 'first_name' => 'required|string|max:255',
