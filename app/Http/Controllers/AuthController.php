8eb1c7e370d5830f761ca5c45578f0684ae95f63
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-06-07 14:11:27 +0600

    add

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -215,12 +215,7 @@ class AuthController extends Controller
     //     }
     // }
 
-    use Illuminate\Http\Request;
-    use Illuminate\Support\Facades\Hash;
-    use Illuminate\Support\Facades\Log;
-    use Illuminate\Support\Facades\Validator;
-    use App\Models\User;
-    use Exception;
+
 
     public function register(Request $request)
     {
