245a4c086736e6d15dd06d3839083c305217268a
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-06-09 14:23:20 +0600

    refine userseeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -11,28 +11,24 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'name' => 'Admin',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('Password1945!'),
             'role' => 'admin',
         ]);
 
         User::create([
-            'name' => 'Editor',
             'email' => 'editor@gmail.com',
             'password' => Hash::make('Password1945!'),
             'role' => 'editor',
         ]);
 
         User::create([
-            'name' => 'Author',
             'email' => 'author@gmail.com',
             'password' => Hash::make('Password1945!'),
             'role' => 'author',
         ]);
 
         User::create([
-            'name' => 'Subscriber',
             'email' => 'subscriber@gmail.com',
             'password' => Hash::make('Password1945!'),
             'role' => 'user',
