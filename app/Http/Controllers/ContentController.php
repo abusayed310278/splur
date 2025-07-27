07fe784fffdf68a81e9cc4ced4971161a7b5761d
Abu Sayed <83999377+abusayed310278@users.noreply.github.com>
2025-07-27 15:10:15 +0600

    add

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -782,22 +782,22 @@ class ContentController extends Controller
                     // Decode image2 if it's a JSON string
                     $image2Array = [];
 
-                    if (!empty($content->image2)) {
-                        if (is_string($content->image2)) {
-                            $decoded = json_decode($content->image2, true);
-                            $image2Array = is_array($decoded) ? $decoded : [];
-                        } elseif (is_array($content->image2)) {
-                            $image2Array = $content->image2;
-                        }
+                if (!empty($content->image2)) {
+                    if (is_string($content->image2)) {
+                        $decoded = json_decode($content->image2, true);
+                        $image2Array = is_array($decoded) ? $decoded : [];
+                    } elseif (is_array($content->image2)) {
+                        $image2Array = $content->image2;
                     }
+                }
 
-                    $image2Urls = array_map(function ($img) {
-                        if (Str::startsWith($img, ['http://', 'https://'])) {
-                            return $img;
-                        }
-                        $cleaned = preg_replace('/[^A-Za-z0-9\-_.\/]/', '', $img);
-                        return url('uploads/content/' . ltrim($cleaned, '/'));
-                    }, $image2Array);
+                $image2Urls = array_map(function ($img) {
+                    if (Str::startsWith($img, ['http://', 'https://'])) {
+                        return $img;
+                    }
+                    $cleaned = preg_replace('/[^A-Za-z0-9\-_.\/]/', '', $img);
+                    return url('uploads/content/' . ltrim($cleaned, '/'));
+                }, $image2Array);
 
                     return [
                         'id' => $content->id,
@@ -813,7 +813,7 @@ class ContentController extends Controller
                         'image1' => $content->image1,
                         // Ensure image2 is always an array, even if null
                         'image2' => $image2Array,
-                        // 'image2_url' => $image2Urls,
+                        'image2_url' => $image2Urls,
                         'advertising_image' => $content->advertising_image,
                         'tags' => $content->tags ? preg_replace('/[^A-Za-z0-9, ]/', '', $content->tags) : null,
                         'created_at' => $content->created_at,
@@ -2180,6 +2180,9 @@ class ContentController extends Controller
 
             // Transform each content item
             $transformedData = $contents->getCollection()->transform(function ($item) {
+
+                
+                
                 return [
                     'id' => $item->id,
                     'heading' => $item->heading,
